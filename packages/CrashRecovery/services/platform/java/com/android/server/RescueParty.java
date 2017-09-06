c3a0a0d170e9218d97c80a737d43b1f796d592f0
maxwen <max.weninger@gmail.com>
2017-09-07 00:58:11 +0200

    base: rescuce party: check isDisabled on all public API
    Change-Id: Iad73bbbe20c1e9c138c262729d7f3c8309727b83
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/CrashRecovery/services/platform/java/com/android/server/RescueParty.java
+++ packages/CrashRecovery/services/platform/java/com/android/server/RescueParty.java
@@ -213,6 +213,7 @@ public class RescueParty {
      * will not be fully booted at this time.
      */
     public static boolean isRecoveryTriggeredReboot() {
+        if (isDisabled()) return false;
         return isFactoryResetPropertySet() || isRebootPropertySet();
     }
 
@@ -253,6 +254,7 @@ public class RescueParty {
      */
     public static void onSettingsProviderPublished(Context context) {
         if (!Flags.deprecateFlagsAndSettingsResets()) {
+            if (isDisabled()) return;
             handleNativeRescuePartyResets();
             ContentResolver contentResolver = context.getContentResolver();
             DeviceConfig.setMonitorCallback(
